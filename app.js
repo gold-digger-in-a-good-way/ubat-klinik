5b385a9a97b4fb8e8b92c8ece775d7c651b6202d
gold-digger-in-a-good-way <librarare02@gmail.com>
2026-01-09 01:50:29 +0800

    Refactor setReminder function and improve notification

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ if ('Notification' in window) {
   Notification.requestPermission();
 }
 
+// Reminder Function
 function setReminder() {
   const namaUbat = document.getElementById("namaUbat").value;
   const masa = document.getElementById("masaUbat").value;
@@ -19,19 +20,21 @@ function setReminder() {
 
   const now = new Date();
   const reminderTime = new Date();
-  const [hour, minute] = masa.split(":");
+  const timeSplit = masa.split(":");
 
-  reminderTime.setHours(hour, minute, 0);
+  reminderTime.setHours(timeSplit[0], timeSplit[1], 0);
 
   let delay = reminderTime.getTime() - now.getTime();
-  if (delay < 0) delay += 24 * 60 * 60 * 1000;
+  if (delay < 0) {
+    delay += 24 * 60 * 60 * 1000;
+  }
 
   setTimeout(() => {
     new Notification("Peringatan Ambil Ubat", {
-      body: `Masa untuk ambil ubat: ${namaUbat}`,
+      body: "Masa untuk ambil ubat: " + namaUbat,
       icon: "icon.png"
     });
   }, delay);
 
-  alert("Reminder berjaya ditetapkan");
+  alert("Reminder ubat berjaya ditetapkan");
 }
