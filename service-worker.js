ca659f9a15d2c27a2de790f3a2cfc24b8a95cb11
gold-digger-in-a-good-way <librarare02@gmail.com>
2026-01-09 01:51:58 +0800

    Replace single quotes with double quotes in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('ubat-klinik-cache').then(cache => {
+    caches.open("ubat-klinik-cache").then(cache => {
       return cache.addAll([
-        'index.html',
-        'style.css',
-        'app.js'
+        "index.html",
+        "style.css",
+        "app.js"
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
